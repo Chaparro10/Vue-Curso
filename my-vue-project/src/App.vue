a3d060ac5a3f4d37505773c560071d0e892c38eb
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-01 13:07:42 -0700

    Directiva v-if

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,52 +1,66 @@
 <script setup>
 import { ref } from 'vue';
 import HeaderComponent from './components/HeaderComponent.vue';
-let number=ref(0);
+let number = ref(0);
 
 //setInterval(()=>{number.value++},100);
 
-const myId="1";
+const myId = "1";
 
-const myValue=ref("some value");
+const myValue = ref("some value");
 
-const myValueModel=ref("Valor inicial");
+const myValueModel = ref("Valor inicial");
 
-const addOne=()=>{
+const addOne = () => {
   number.value++;
 }
 
-let myCondition=true;
+let myCondition = true;
+
+const condition = false;
 
 </script>
 
 <template>
-  <HeaderComponent/>
-      <h1 v-bind:id="myId" class="title">Hola Mundo!</h1>
+  <HeaderComponent />
+  <h1 v-bind:id="myId" class="title">Hola Mundo!</h1>
 
-      <h2 @click="addOne">{{ number }}</h2>
+  <h2 @click="addOne">{{ number }}</h2>
 
-      <input type="text" :value="myValue">
+  <input type="text" :value="myValue">
 
-      <h3>{{ myValueModel }}</h3>
+  <h3>{{ myValueModel }}</h3>
 
-      <input type="text" v-model="myValueModel">
+  <input type="text" v-model="myValueModel">
 
-      <h1 :class="{highlight:myCondition,red:true}">
-          Texto condicional
-      </h1>
+  <h1 :class="{ highlight: myCondition, red: true }">
+    Texto condicional
+  </h1>
 
-</template> 
+  <h1>Directiva v-if</h1>
+
+  <h2 v-if="condition">la condicion se cumple</h2>
+  <h2 v-else-if="condition==1">
+    el else if
+  </h2>
+  <h2 v-else>
+    no se cumple ninguna condicion
+  </h2>
+
+</template>
 
 <style scoped>
-.title{
+.title {
   background-color: blue;
   color: white;
 }
-.highlight{
+
+.highlight {
   background-color: black;
   color: yellow;
 }
-.red{
+
+.red {
   color: red;
 }
 </style>
