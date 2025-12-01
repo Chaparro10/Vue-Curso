6dd005c729bf2543ef77c85ecc067ffe7ce75195
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-01 12:28:02 -0700

    creacion de componentes

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+
+</script>
 
 <template>
+  <HeaderComponent/>
       <h1 class="title">Hola Mundo!</h1>
-</template>
 
-<style>
+</template> 
+
+<style scoped>
 .title{
   background-color: blue;
   color: white;
