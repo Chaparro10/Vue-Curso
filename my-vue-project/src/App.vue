ba053d9b209a2af144223e336b74afb1ace6ebc3
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-01 12:38:12 -0700

    eventos

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -3,14 +3,21 @@ import { ref } from 'vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 let number=ref(0);
 
-setInterval(()=>{number.value++},100)
+//setInterval(()=>{number.value++},100);
+
+
+
+const addOne=()=>{
+  number.value++;
+}
 
 </script>
 
 <template>
   <HeaderComponent/>
       <h1 class="title">Hola Mundo!</h1>
-      <h2>{{ number }}</h2>
+
+      <h2 @click="addOne">{{ number }}</h2>
 
 </template> 
 
