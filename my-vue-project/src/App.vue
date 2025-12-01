5a825cd8fb7ceb55a2a8164de8061aeb5b7d3747
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-01 12:34:37 -0700

    variables reactivas

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from 'vue';
 import HeaderComponent from './components/HeaderComponent.vue';
+let number=ref(0);
+
+setInterval(()=>{number.value++},100)
 
 </script>
 
 <template>
   <HeaderComponent/>
       <h1 class="title">Hola Mundo!</h1>
+      <h2>{{ number }}</h2>
 
 </template> 
 
