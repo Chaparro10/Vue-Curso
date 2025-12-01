3aacaee085945a76d34e2a9d665854d95ce95347
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-01 13:16:07 -0700

    Directiva v-for

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from 'vue';
-import HeaderComponent from './components/HeaderComponent.vue';
+import { ref } from "vue";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import { animals } from "./data/animals";
 let number = ref(0);
 
 //setInterval(()=>{number.value++},100);
@@ -13,12 +14,12 @@ const myValueModel = ref("Valor inicial");
 
 const addOne = () => {
   number.value++;
-}
+};
 
 let myCondition = true;
 
 const condition = false;
-
+const movies = ["1", "2", "3", "4", 6];
 </script>
 
 <template>
@@ -27,26 +28,32 @@ const condition = false;
 
   <h2 @click="addOne">{{ number }}</h2>
 
-  <input type="text" :value="myValue">
+  <input type="text" :value="myValue" />
 
   <h3>{{ myValueModel }}</h3>
 
-  <input type="text" v-model="myValueModel">
+  <input type="text" v-model="myValueModel" />
 
-  <h1 :class="{ highlight: myCondition, red: true }">
-    Texto condicional
-  </h1>
+  <h1 :class="{ highlight: myCondition, red: true }">Texto condicional</h1>
 
   <h1>Directiva v-if</h1>
 
   <h2 v-if="condition">la condicion se cumple</h2>
-  <h2 v-else-if="condition==1">
-    el else if
-  </h2>
-  <h2 v-else>
-    no se cumple ninguna condicion
-  </h2>
+  <h2 v-else-if="condition == 1">el else if</h2>
+  <h2 v-else>no se cumple ninguna condicion</h2>
 
+  <h1>Directiva v-for</h1>
+
+  <div>
+    <p v-for="(value, index) in movies" :key="value">
+      {{ index + 1 }}-{{ value }}
+    </p>
+
+    <p>Animales</p>
+     <p v-for="(value, index) in animals" :key="value.id">
+      {{ index + 1 }}-{{ value.name }}
+    </p>
+  </div>
 </template>
 
 <style scoped>
---
+++ my-vue-project/src/data/animals.js
@@ -0,0 +1,16 @@
+
+export const animals=[
+    {
+        id:1,
+        name:"dog",
+        img:""
+    },
+    {
+        id:2,
+        name:"cat"
+    },
+    {
+        id:3,
+        name:"bird"
+    }
+];
