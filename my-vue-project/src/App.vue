ab882778490eefb5df816d2fce06b682ac4101ca
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-02 10:09:32 -0700

    formularios

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref, watch, watchEffect } from "vue";
+import { computed, reactive, ref, watch, watchEffect } from "vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
 import { animals } from "./data/animals";
 import ChildComponent from "./components/ChildComponent.vue";
@@ -23,49 +23,66 @@ const condition = false;
 const movies = ["1", "2", "3", "4", 6];
 const count = ref(5);
 
-const addCount=()=>{
+const addCount = () => {
   count.value++;
 }
 
-const displayName=ref('');
+const displayName = ref('');
 
-const setName=(event)=>{
-  displayName.value=event;
+const setName = (event) => {
+  displayName.value = event;
 }
 
 
-const namePerson1=ref("JoseJuan");
+const namePerson1 = ref("JoseJuan");
 
-const namePerson1Length= computed(()=>{
+const namePerson1Length = computed(() => {
   return namePerson1.value.length;
 });
 
-const userName=ref({name:"Adrian"});
-const setName1=()=>{
-  userName.value.name="Maria";
+const userName = ref({ name: "Adrian" });
+const setName1 = () => {
+  userName.value.name = "Maria";
 }
 
-watch(userName,(newValue,oldValue)=>{
+watch(userName, (newValue, oldValue) => {
   console.log('newValue', newValue)
   console.log('oldValue', oldValue)
   console.log('Username modified')
 }, {
- immediate: true,//para que se ejecute en cuanto inicia
- deep:true // para objetos
+  immediate: true,//para que se ejecute en cuanto inicia
+  deep: true // para objetos
 })
 
-watchEffect(()=>{//Se ejecuta al iniciar
+watchEffect(() => {//Se ejecuta al iniciar
   console.log('Username modified to:', userName.value)
 })
+
+//ref-> para valores primitivo o una sola variable
+//reactive-> para objetos o estructuras complejas
+
+const movie = reactive({
+  title: "",
+  duration: 0,
+  director: 0
+})
+
+const handleSubmit=()=>{
+  if(!movie.title || !movie.duration || ! movie.director){
+    alert("Faltan campos por llenar");
+    return;
+  }
+    console.log('movie', movie)
+}
 
 </script>
 
 <template>
   <!-- Pasar props componente padre a hijo -->
   <button @click="addCount">
-      sumar 1
+    sumar 1
   </button>
-  <HeaderComponent  message="Hola desde el padre" v-bind:count="count" />
+  <HeaderComponent message="Hola desde el padre" v-bind:count="count" />
   <h1 v-bind:id="myId" class="title">Hola Mundo!</h1>
 
   <!-- Directiva v-bind -->
@@ -78,7 +95,7 @@ watchEffect(()=>{//Se ejecuta al iniciar
 
   <input type="text" v-model="myValueModel" />
 
-  <h1>longitud texto:{{namePerson1Length}}</h1>
+  <h1>longitud texto:{{ namePerson1Length }}</h1>
   <input type="text" v-model="namePerson1">
 
   <!-- clases css dinamicas -->
@@ -99,48 +116,64 @@ watchEffect(()=>{//Se ejecuta al iniciar
     </p>
 
     <p>Animales</p>
-     <p v-for="(value, index) in animals" :key="value.id">
+    <p v-for="(value, index) in animals" :key="value.id">
       {{ index + 1 }}-{{ value.name }}
     </p>
   </div>
 
 
   <!-- Comunicacion hijo- padre -->
-<h1>Hola {{ displayName }}</h1>
+  <h1>Hola {{ displayName }}</h1>
   <ChildComponent @login="setName">
     <h3>Contenido desde el componente padre como slot</h3>
   </ChildComponent>
 
   <!-- SLOT -->
-  <ChildComponent >
+  <ChildComponent>
   </ChildComponent>
 
-   <ChildComponent >
-      <template v-slot:top>
-          <h1>TOP</h1>
-      </template>
-       <template v-slot:bottom>
-          <h1>BOTTOM</h1>
-      </template>
+  <ChildComponent>
+    <template v-slot:top>
+      <h1>TOP</h1>
+    </template>
+    <template v-slot:bottom>
+      <h1>BOTTOM</h1>
+    </template>
   </ChildComponent>
 
   <!-- WATCH  | VIGILANCIA DE CAMBIOS -->
-    <h1>
-      Watch | Vigilancia de cambios
-    </h1>
-    <h2>Hola, {{userName.name}}</h2>
+  <h1>
+    Watch | Vigilancia de cambios
+  </h1>
+  <h2>Hola, {{ userName.name }}</h2>
 
-    <button @click="setName1">
-      Cambiar de usuario
+  <button @click="setName1">
+    Cambiar de usuario
+  </button>
+
+  <!-- WATCH EFFECT | VIGILANCIA DE CAMBIOS -->
+
+
+
+  <!--  Formularios -->
+  <h1>Formss</h1>
+  <form @submit.prevent="handleSubmit" class="form">
+    <label for="title">Title</label>
+    <input v-model="movie.title" type="text" id="title">
+
+    <label for="duration">Duration</label>
+    <input v-model="movie.duration" type="number" id="duration">
+
+    <label for="director">Director</label>
+    <input v-model="movie.director" type="text" id="director">
+
+    <button>
+      Enviar
     </button>
- 
-    <!-- WATCH EFFECT | VIGILANCIA DE CAMBIOS -->
 
-    
-     
+  </form>
 
 
-  
 </template>
 
 <style scoped>
@@ -156,5 +189,11 @@ watchEffect(()=>{//Se ejecuta al iniciar
 
 .red {
   color: red;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 </style>
