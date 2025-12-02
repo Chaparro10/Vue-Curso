4ecd284b70b0bdc223b0de8ac66804651161b647
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-02 16:26:55 -0700

    separacion por componentes

--- my-note/src/views/Note.vue
+++ my-note/src/views/Note.vue
@@ -1,9 +1,47 @@
-<script setup></script>
+<script setup>
+import NoteCard from '../components/NoteCard.vue';
+import { useNoteStore } from '../stores/note';
+
+
+const noteStore=useNoteStore();
+
+</script>
 <template>
-    <h2>
-        Notes page
-    </h2>
+ <section id="notes-page">
+        <h2>Notes</h2>
+
+        <ul class="note-list">
+            <li v-for="note in noteStore.notes" :key="note.id">    
+               <NoteCard :note="note"/>
+            </li>
+        </ul>
+
+ </section>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+
+#notes-page{
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .title{
+        margin-bottom: 50px;
+        text-align: center;
+    }
+    .note-list{
+        background-image: url("/assets/cork.jpg");
+        border: 10px solid brown;
+        width: 70%;
+        min-width: 450px;
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+    }
+}
+</style>
