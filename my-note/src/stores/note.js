c4a423220f77643accdf9b034bb0fb847b1235b7
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-12-03 09:34:56 -0700

    obtener post

--- my-note/src/stores/note.js
+++ my-note/src/stores/note.js
@@ -1,9 +1,9 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-
-
 export const useNoteStore = defineStore("Note", () => {
+
+    const URL_API="https://jsonplaceholder.typicode.com/posts";
   const notes = ref([
     {
       id: "1",
@@ -17,9 +17,27 @@ export const useNoteStore = defineStore("Note", () => {
     },
   ]);
 
-  const addNote = (note) => {
+  const addNote = (title) => {
+    const note = {
+      id: notes.value.length++,
+      title: title,
+      marked: false,
+    };
     notes.value.push(note);
   };
 
-  return { notes, addNote };
+
+  const getPost=async()=>{
+    try{
+ const response= await fetch(URL_API);
+    const data = await response.json();
+    console.log('data', data)
+   return data
+    }catch(error){
+            console.log('error', error)
+    }
+   
+  }
+
+  return { notes, addNote,getPost };
 });
--- my-note/src/views/Note.vue
+++ my-note/src/views/Note.vue
@@ -1,47 +1,62 @@
 <script setup>
-import NoteCard from '../components/NoteCard.vue';
-import { useNoteStore } from '../stores/note';
+import CreateNote from "../components/CreateNote.vue";
+import NoteCard from "../components/NoteCard.vue";
+import { useNoteStore } from "../stores/note";
+import { ref, onMounted } from "vue";
 
-
-const noteStore=useNoteStore();
-
+const noteStore = useNoteStore();
+let posts = ref([]);
+onMounted(async () => {
+  posts.value = await noteStore.getPost();
+  console.log("posts", posts);
+});
 </script>
 <template>
- <section id="notes-page">
-        <h2>Notes</h2>
+  <section id="notes-page">
+    <h2>Notes</h2>
 
-        <ul class="note-list">
-            <li v-for="note in noteStore.notes" :key="note.id">    
-               <NoteCard :note="note"/>
-            </li>
-        </ul>
+    <ul class="note-list">
+      <li v-for="note in noteStore.notes" :key="note.id">
+        <NoteCard :note="note" />
+      </li>
+      <li>
+        <CreateNote />
+      </li>
+    </ul>
 
- </section>
+    <h2>Posts</h2>
+
+    <ul class="note-list">
+      <li v-for="post in posts" :key="post.id">
+        <p>
+          {{ post.title }}
+        </p>
+      </li>
+    </ul>
+  </section>
 </template>
 
-
 <style scoped>
+#notes-page {
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-#notes-page{
-    padding: 40px;
+  .title {
+    margin-bottom: 50px;
+    text-align: center;
+  }
+  .note-list {
+    background-image: url("/assets/cork.jpg");
+    border: 10px solid brown;
+    width: 70%;
+    min-width: 450px;
+    padding: 30px;
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    .title{
-        margin-bottom: 50px;
-        text-align: center;
-    }
-    .note-list{
-        background-image: url("/assets/cork.jpg");
-        border: 10px solid brown;
-        width: 70%;
-        min-width: 450px;
-        padding: 30px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 20px;
-    }
+    gap: 20px;
+  }
 }
 </style>
