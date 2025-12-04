37b62d7742d4b56bb3f35d8d4d283b660d015a50
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-12-03 18:40:49 -0700

    formulario completo
    vuetify:para componentes
    
    vuevalidate:para validar formularios

--- contact-form/src/main.js
+++ contact-form/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import "vuetify/styles"; //estilos basicos
+import { aliases, mdi } from "vuetify/iconsets/mdi"; //iconos
+import "@mdi/font/css/materialdesignicons.css"; // Iconos MDI
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: { mdi },
+  },
+});
+
+createApp(App).use(vuetify).mount("#app");
